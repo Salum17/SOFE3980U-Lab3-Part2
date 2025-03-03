e7e9dffa932e472e2c8ab899259285e3bd53e0a5
Salum17 <106616585+Salum17@users.noreply.github.com>
2025-03-03 12:58:19 -0500

    Add files via upload

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryAPIControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryAPIControllerTest.java
@@ -48,4 +48,116 @@ public class BinaryAPIControllerTest {
 			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(10001))
 			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
     }
-}
+    @Test
+    public void addwithzero() throws Exception {
+        this.mvc.perform(get("/add_json").param("operand1","111").param("operand2","0"))//adding with zero
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
+    }
+    @Test
+    public void carryover() throws Exception {
+        this.mvc.perform(get("/add_json").param("operand1","111").param("operand2","111"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1110))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
+    }
+    @Test
+    public void differentlengths() throws Exception {
+        this.mvc.perform(get("/add_json").param("operand1","111").param("operand2","10100"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(10100))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(11011))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("add"));
+    }
+    @Test
+    public void differentlengthsor() throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","111").param("operand2","10100"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(10100))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(10111))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+    @Test
+    public void or0s () throws Exception {
+        this.mvc.perform(get("/or_json").param("operand1","11110").param("operand2","00000"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11110))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(00000))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(11110))
+			.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+    }
+    @Test
+    public void mult() throws Exception{
+        this.mvc.perform(get("/multiply_json").param("operand1", "1001").param("operand2", "1011"))
+        .andExpect(status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(1001))//9
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(1011))//11
+		.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(1100011))//9x11 should equal to 99
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+
+    }
+    @Test
+    public void mult1s() throws Exception{
+        this.mvc.perform(get("/multiply_json").param("operand1", "111").param("operand2", "111"))
+        .andExpect(status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(111))//7
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(111))//7
+		.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(110001))//7x7 should equal to 49
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+
+    }
+    @Test
+    public void mult0difflength() throws Exception{
+        this.mvc.perform(get("/multiply_json").param("operand1", "111111").param("operand2", "0000"))
+        .andExpect(status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(111111))//63
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0000))//0
+		.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(000000))//63x0 should equal to 0
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("multiply"));
+
+    }
+    @Test
+    public void or () throws Exception{
+        this.mvc.perform(get("/or_json").param("operand1", "110010").param("operand2", "100100"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(110010))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(100100))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(110110))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("or"));
+
+    }
+    @Test
+    public void and () throws Exception{
+        this.mvc.perform(get("/and_json").param("operand1", "11010").param("operand2", "11001"))
+        .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11010))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(11001))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(11000))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+
+    }
+    @Test
+    public void and0difflength() throws Exception{
+        this.mvc.perform(get("/and_json").param("operand1", "11111").param("operand2", "0000"))
+        .andExpect(status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11111))//31
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(0000))//0
+		.andExpect(MockMvcResultMatchers.jsonPath("$.result").value(000000))//31x0 should equal to 0
+		.andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));}
+@Test
+        public void and1s() throws Exception{
+            this.mvc.perform(get("/and_json").param("operand1", "11111").param("operand2", "11111"))
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand1").value(11111))//31
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operand2").value(11111))//31
+            .andExpect(MockMvcResultMatchers.jsonPath("$.result").value(11111))//31&31 should equal to 31
+            .andExpect(MockMvcResultMatchers.jsonPath("$.operator").value("and"));
+    
+        }
+    }
+
--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
@@ -56,5 +56,132 @@ public class BinaryControllerTest {
 			.andExpect(model().attribute("result", "1110"))
 			.andExpect(model().attribute("operand1", "111"));
     }
+    @Test
+	    public void difflength() throws Exception {
+        this.mvc.perform(post("/").param("operand1","111").param("operator","+").param("operand2","01110"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+			.andExpect(model().attribute("result", "10101"))
+			.andExpect(model().attribute("operand1", "111"))
+            .andExpect(model().attribute("operand2", "01110"));
+    }
+    @Test
+	    public void zero() throws Exception {
+        this.mvc.perform(post("/").param("operand1","111").param("operator","+").param("operand2","000"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+			.andExpect(model().attribute("result", "111"))
+			.andExpect(model().attribute("operand1", "111"))
+            .andExpect(model().attribute("operand2", "000"));
+    }
+    @Test
+	    public void carry() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1111").param("operator","+").param("operand2","1111"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+			.andExpect(model().attribute("result", "11110"))
+			.andExpect(model().attribute("operand1", "1111"))
+            .andExpect(model().attribute("operand2", "1111"));
+    }
+    @Test
+	    public void postParameteror() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","|").param("operand2","1001"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+			.andExpect(model().attribute("result", "1011"))
+			.andExpect(model().attribute("operand1", "1010"))
+            .andExpect(model().attribute("operand2", "1001"));
+    }
+    @Test
+	    public void postParameterorzero() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","|").param("operand2","0000"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+			.andExpect(model().attribute("result", "1010"))
+			.andExpect(model().attribute("operand1", "1010"))
+            .andExpect(model().attribute("operand2", "0000"));
+    }
+    @Test
+    public void postParameterorall1() throws Exception {
+    this.mvc.perform(post("/").param("operand1","1111").param("operator","|").param("operand2","1111"))//.andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "1111"))
+        .andExpect(model().attribute("operand1", "1111"))
+        .andExpect(model().attribute("operand2", "1111"));
+}
+    @Test
+    public void ordifflength() throws Exception {
+    this.mvc.perform(post("/").param("operand1","111100").param("operator","|").param("operand2","1111"))//.andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "111111"))
+        .andExpect(model().attribute("operand1", "111100"))
+        .andExpect(model().attribute("operand2", "1111"));
+}
+@Test
+public void postParameterandall1() throws Exception {
+this.mvc.perform(post("/").param("operand1","111111").param("operator","&").param("operand2","111111"))//.andDo(print())
+    .andExpect(status().isOk())
+    .andExpect(view().name("result"))
+    .andExpect(model().attribute("result", "111111"))
+    .andExpect(model().attribute("operand1", "111111"))
+    .andExpect(model().attribute("operand2", "111111"));
+}
+@Test
+public void postParameterandzero() throws Exception {
+this.mvc.perform(post("/").param("operand1","10101011").param("operator","&").param("operand2","00000000"))//.andDo(print())
+    .andExpect(status().isOk())
+    .andExpect(view().name("result"))
+    .andExpect(model().attribute("result", "0"))
+    .andExpect(model().attribute("operand1", "10101011"))
+    .andExpect(model().attribute("operand2", "00000000"));
+}
+@Test
+public void postParametermultzero() throws Exception {
+this.mvc.perform(post("/").param("operand1","110111").param("operator","*").param("operand2","000000"))//.andDo(print())
+    .andExpect(status().isOk())
+    .andExpect(view().name("result"))
+    .andExpect(model().attribute("result", "0"))
+    .andExpect(model().attribute("operand1", "110111"))
+    .andExpect(model().attribute("operand2", "000000"));
+}
+@Test
+public void postParametermultdifflength() throws Exception {
+this.mvc.perform(post("/").param("operand1","1111").param("operator","*").param("operand2","011"))//.andDo(print())
+    .andExpect(status().isOk())
+    .andExpect(view().name("result"))
+    .andExpect(model().attribute("result", "101101"))
+    .andExpect(model().attribute("operand1", "1111"))
+    .andExpect(model().attribute("operand2", "011"));
+}
+@Test
+public void postParametermult1s() throws Exception {
+this.mvc.perform(post("/").param("operand1","111").param("operator","*").param("operand2","111"))//.andDo(print())
+    .andExpect(status().isOk())
+    .andExpect(view().name("result"))
+    .andExpect(model().attribute("result", "110001"))
+    .andExpect(model().attribute("operand1", "111"))
+    .andExpect(model().attribute("operand2", "111"));
+}
+    @Test
+	    public void postParameterand() throws Exception {
+        this.mvc.perform(post("/").param("operand1","10110").param("operator","&").param("operand2","11010"))//.andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("result"))
+			.andExpect(model().attribute("result", "10010"))
+			.andExpect(model().attribute("operand1", "10110"))
+            .andExpect(model().attribute("operand2", "11010"));
+    }
+    @Test
+    public void postParametermult() throws Exception {
+    this.mvc.perform(post("/").param("operand1","1110").param("operator","*").param("operand2","1010"))//.andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(view().name("result"))
+        .andExpect(model().attribute("result", "10001100"))
+        .andExpect(model().attribute("operand1", "1110"))
+        .andExpect(model().attribute("operand2", "1010"));
+}
+
 
 }
